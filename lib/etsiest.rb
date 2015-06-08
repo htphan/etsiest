4ea9e622d2b7b46c9b716c5f1e21564e334b9c65
Henry Phan <hphan.cs@gmail.com>
2015-06-08 00:04:38 -0400

    Added search route, erb, and css

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,18 @@
+require "sinatra" 
 require "etsiest/version"
+require "etsy"
+require "json"
+require "pry"
+
+Etsy.protocol = "https"
+Etsy.api_key = ENV['ETSY_KEY']
+enable :logging
 
 module Etsiest
-  # Your code goes here...
+end
+
+get '/etsy_search' do
+  response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => "#{params['q']}")
+  @results = response.to_hash['results']
+  erb :etsy_search
 end
